c775d8073f0cac9fa75627e5db69c9b3e533a5d1
anurag1302 <anurag3487@gmail.com>
2026-05-10 14:05:37 +0530

    chunking code - revisit

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import json
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
+from src.chunks import split_text
 import logging
 
 app = FastAPI(title="RAG Fast API")
@@ -20,4 +21,5 @@ async def upload(file: UploadFile = File()):
     text = content.decode("utf-8")
     logger.info(f"File name:{file.filename}, File size:{len(content)}")
     logger.info(f"File content:{text[:400]}")
+    split_text(text, chunk_size=500, chunk_overlap=100)
     return "Success"
