1ca626e395d4690e8df0b25d2ab28eef76d637f7
anurag1302 <anurag3487@gmail.com>
2026-05-12 22:44:14 +0530

    uploadFile function on button click

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,30 @@ function App() {
   const [file, setFile] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const uploadFile = async () => {
+    if (!file) {
+      alert("Please upload a file!");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      setLoading(true);
+      console.log(file);
+      console.log(formData);
+    } catch (error) {
+      console.log(error);
+      alert("Upload Failed");
+    }
+    setLoading(false);
+  };
+
   return (
     <>
       <h1>RAG Frontend</h1>
       <h2>Upload text file</h2>
       <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+      <button onClick={uploadFile}>Upload</button>
     </>
   );
 }
