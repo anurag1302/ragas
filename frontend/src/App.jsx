cede1a8394b2194d90a19b8c2f8663724248a06b
anurag1302 <anurag3487@gmail.com>
2026-05-12 22:34:09 +0530

    added upload button and state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [question, setQuestion] = useState("");
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   return (
     <>
       <h1>RAG Frontend</h1>
+      <h2>Upload text file</h2>
+      <input type="file" onChange={(e) => setFile(e.target.files[0])} />
     </>
   );
 }
